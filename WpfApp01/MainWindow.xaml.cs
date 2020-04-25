2ef2d1651cb0de04c92a470fb5533d421716dc83
yishizu <48244588+yishizu@users.noreply.github.com>
2020-04-26 02:23:37 +0900

    Initial commit

---
+++ WpfApp01/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirsWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid grid = new Grid();
+            this.Content = grid;
+            Button btn = new Button();
+            btn.FontSize = 20;
+            btn.Height = 35;
+            btn.Width = 400;
+            WrapPanel wp = new WrapPanel();
+            TextBlock tb = new TextBlock();
+            tb.Text = "This is Button";
+            tb.Foreground = Brushes.Red;
+            wp.Children.Add(tb);
+
+            tb = new TextBlock();
+            tb.Text = "Next buttonText";
+            tb.Foreground = Brushes.Blue;
+            wp.Children.Add(tb);
+
+            tb = new TextBlock();
+            tb.Text = "Last buttonText";
+            tb.Foreground = Brushes.Black;
+            wp.Children.Add(tb);
+
+            btn.Content = wp;
+            grid.Children.Add(btn);
+
+
+        }
+    }
+}
---
+++ WpfApp02/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thanks for clicking me");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Direct Event");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse up button triggerd. Only Right Button Worked");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("It worked with both!");
+        }
+
+        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Rigth Button Up");
+        }
+
+        private void Button_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Left Button up");
+        }
+    }
+}
