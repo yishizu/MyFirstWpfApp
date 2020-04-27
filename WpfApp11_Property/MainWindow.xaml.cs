b71eaf7563ac63376e0912a4a7e073f39ffd71d6
yishizu <48244588+yishizu@users.noreply.github.com>
2020-04-28 02:23:31 +0900

    Add Password

--- WpfApp11_Property/MainWindow.xaml.cs
+++ WpfApp11_Property/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp11_Property
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcome " + tbUsername.Text);
+        }
     }
 }
